d9df4823d02218c07a02fe4ef87d16354d66abe9
LuizinSouza <henriquesouzasn10@gmail.com>
2025-09-04 15:25:04 -0300

    endpoint Vizualizar dados do Cliente

---
+++ src/main/java/br/com/luizsn/VassCommerce/controller/ClienteController.java
@@ -0,0 +1,28 @@
+package br.com.luizsn.VassCommerce.controller;
+
+
+
+
+import br.com.luizsn.VassCommerce.model.Cliente;
+import br.com.luizsn.VassCommerce.service.ClienteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ClienteController {
+    private final  ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService){
+        this.clienteService = clienteService;
+    }
+
+    //Get /CLIENTE/{id}/ -> visualizar dados do cliente
+
+    @GetMapping("/cliente/{id}")
+    public Cliente VisuzalizarDadosCliente(@PathVariable int id){
+        return clienteService.dadosCliente(id);
+    }
+}
---
+++ src/main/java/br/com/luizsn/VassCommerce/service/ClienteService.java
@@ -0,0 +1,38 @@
+package br.com.luizsn.VassCommerce.service;
+
+import br.com.luizsn.VassCommerce.model.Cliente;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Service
+
+public class ClienteService {
+    private final List<Cliente> clientes =  new ArrayList<>();
+
+    LocalDate dataNascimento = LocalDate.of(2005, 3, 3);
+    public ClienteService(){
+        clientes.add(new Cliente(
+                1,
+                "Luiz Henrique",
+                "luiz@gmail.com",
+                "1234",
+                new Date(),new Date(),
+                "",
+                dataNascimento,
+                "1234567"
+        ));
+    }
+
+    //visualizar dados do cliente pelo ID
+
+    public Cliente dadosCliente(int id){
+        return clientes.stream()
+                .filter(c -> c.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+}
