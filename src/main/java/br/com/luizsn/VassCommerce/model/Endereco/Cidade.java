12bd31e188cb510ccc03f1ae2ddb1d5181a9ae8a
LuizinSouza <henriquesouzasn10@gmail.com>
2025-09-09 16:13:35 -0300

    feat: adição do endpoint listar endereço do cliente

---
+++ src/main/java/br/com/luizsn/VassCommerce/model/Endereco/Cidade.java
@@ -0,0 +1,38 @@
+package br.com.luizsn.VassCommerce.model.Endereco;
+
+public class Cidade {
+
+    private long id;
+    private String nome;
+    private Estado estado;
+
+    public Cidade(long id, String nome, Estado estado) {
+        this.id = id;
+        this.nome = nome;
+        this.estado = estado;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+}
